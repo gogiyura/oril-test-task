2478d61584f5082e5a27e0b0ac6ae3b9aa7bc516
gogiyura <gogiyur@gmail.com>
2022-03-23 19:32:16 +0200

    generated spring project added

---
+++ src/main/java/com/example/oriltesttask/OrilTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.oriltesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrilTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrilTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oriltesttask/OrilTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oriltesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrilTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
